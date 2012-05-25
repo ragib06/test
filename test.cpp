548b92640e95cf4c7659c7e79a4af25627a79fa0
Ragib Ahsan <ragib.ahsan@gmail.com>
2012-05-25 12:07:05 +0600

    First commit

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <sys/timeb.h>
+using namespace std;
+
+int main()
+{
+    printf("Hello World\n");
+    return 0;
+}
---
+++ test.java
@@ -0,0 +1,10 @@
+import java.util.Vector;
+import java.util.Random;
+
+public class test{
+	
+    public static void main(String args[]){
+        System.out.println("hello World!"); 
+    }
+    
+}
